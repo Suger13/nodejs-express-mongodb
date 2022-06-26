57421a731a1d96e0d691b432273896da6fa050f4
Suger13 <muteki.scarlette@gmail.com>
2022-06-26 15:12:03 +0700

    modifiedproduct.controllerm product.route

--- src/module/route/product.route.js
+++ src/module/route/product.route.js
@@ -5,6 +5,5 @@ const productController = require('../controller/product.controller');
 route.get('/', productController.getProduct);
 route.get('/:id', productController.getProductById);
 route.post('/', productController.createProduct)
-// route.post('/', productController.putProduct)
 
 module.exports = route;
